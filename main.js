18a5656a7ce12f25c45212413d304598b3281eff
JuanOlvera06 <20240982@uthh.edu.mx>
2026-02-16 17:34:11 -0600

    TERMINACION DE PRACTICA

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const cloudname=""
-const preset=""
+const cloudname="dstrcicli"
+const preset="preset_5C"
 
 const inputf=document.getElementById("fileinput")
 const imagen=document.getElementById("imagen")
+const btn =document.getElementById("subir")
 
 const subirimg=()=>{
+//desabilitar boton
+
     const foto=inputf.files[0]
+    if (!foto) return alert("Selecciona una imagen"); //validar que haya imagen
+
+    btn.disabled=true;
+btn.classList.add("opacity-50", "cursor-not-allowed")
+
+
+    const loader = document.getElementById("loading-status");//indicador del reusltado
+    loader.classList.remove("hidden"); //Tailwind usa la clase hidden para aplicar un display: none
+// cuando la quito aparece depsues de preisonar el boton
 
     const formdata= new FormData()
     formdata.append('file',foto)
@@ -24,5 +36,20 @@ const subirimg=()=>{
         alert("Imagen subida con exito")
         imagen.src=data.secure_url
     })
+    .catch(error => {
+        // Capturar errores de red o de proceso
+        console.error("Error detectado:", error);
+    })
+    .finally(() => {
+        loader.classList.add("hidden")
+        btn.disabled=false;
+        btn.classList.remove("opacity-50", "cursor-not-allowed")
+    });
     
 }
+
+
+//const cloudname="dstrcicli"
+//const preset = "preset_5C"
+
+
